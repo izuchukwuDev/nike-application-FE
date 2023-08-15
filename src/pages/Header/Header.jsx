30230f45625a97c4752d3dbb391751a136d2e359
izu20 <izuokoye20@gmail.com>
2023-08-15 14:06:13 +0100

    project scaffolding

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div>Header Section</div>;
 };
 
 export default Header;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return <div>Main Body</div>;
 };
 
 export default Main;
